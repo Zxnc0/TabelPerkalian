ff20bef5078cb6613ba61095a4d592bc6062ea4c
Nur Ridwan <130064161+Zxnc0@users.noreply.github.com>
2023-11-17 15:29:35 +0700

    Update setup.py

--- Tabel Perkalian/setup.py
+++ Tabel Perkalian/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PerkalianIwan',
+    name='tabelperkalianiwan404',
     version='0.0.1',
     packages=find_packages(),
     description='Tabel perkalian simpel, yang membuat saya pusing menguploadnya ke pypi',
